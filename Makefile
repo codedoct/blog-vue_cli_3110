9aae58b534de6593cb6fdf9e032f61ed00493eb6
victra <victra.sonata@gmail.com>
2020-10-20 22:09:06 +0700

    ini repo

---
+++ Makefile
@@ -0,0 +1,5 @@
+run_setup:
+	npm install
+
+run_local:
+	npm run serve
--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import VeeValidate from 'vee-validate';
+import App from './App.vue';
+import router from './router';
+import { store } from './store';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VeeValidate);
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');
